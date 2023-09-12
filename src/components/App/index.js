8c1f7f5abd38f50f5135bf604b49979bceac237b
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-09-12 19:07:43 -0300

    feat: add contact list header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
+import ContactList from '../ContactList';
 
 import { Container } from './styles';
 
@@ -13,6 +14,7 @@ function App() {
       <GlobalStyles />
       <Container>
         <Header />
+        <ContactList />
       </Container>
     </ThemeProvider>
   );
