d0c86c56c42bd1b3296abcd1a68310884ca2e53b
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-09-13 18:09:29 -0300

    feat: add Button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -12,6 +13,7 @@ export default function NewContact() {
         <option value="instagram">Instagram</option>
         <option value="instagram">Instagram</option>
       </Select>
+      <Button type="button">Cadastrar</Button>
     </>
   );
 }
