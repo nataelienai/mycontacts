70ae293d338dede2d3d172a0565c8ee4da8345c3
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-09-13 17:52:04 -0300

    feat: add Input and Select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
+      </Select>
+    </>
   );
 }
