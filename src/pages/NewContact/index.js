dc703c52fea12a454e487612a074394875d5e50b
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-09-13 18:38:18 -0300

    feat: add ContactForm component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,19 +1,11 @@
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
+import ContactForm from '../../components/ContactForm';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <Input type="text" placeholder="Nome" />
-      <Select>
-        <option value="instagram">Instagram</option>
-        <option value="instagram">Instagram</option>
-        <option value="instagram">Instagram</option>
-      </Select>
-      <Button type="button">Cadastrar</Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
