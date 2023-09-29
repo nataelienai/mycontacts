caecb1fc7d41728577751ca2bfe73736e19a211d
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-09-29 14:13:29 -0300

    feat: animate Modal entrance

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   position: fixed;
@@ -11,6 +31,7 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +41,7 @@ export const Container = styled.div`
   box-shadow: 0px 4px 10px rgb(0, 0, 0, 0.04);
   border-radius: 4px;
   padding: 24px;
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
