96fb5eba049c778e9fe53336b23f12bd574eb4d5
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-04 19:29:22 -0300

    fix: replace Switch with Routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import Routes from '../../Routes';
+import Router from '../../Router';
 
 import { Container } from './styles';
 import ToastContainer from '../Toast/ToastContainer';
@@ -19,7 +19,7 @@ function App() {
 
         <Container>
           <Header />
-          <Routes />
+          <Router />
         </Container>
       </ThemeProvider>
     </BrowserRouter>
