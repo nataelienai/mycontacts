b9f8e8238da8a996325135470f026f4a129c6517
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2024-01-15 13:02:44 -0300

    feat: make theme persistent with local storage

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,12 +1,24 @@
 import {
-  createContext, useCallback, useMemo, useState,
+  createContext, useCallback, useEffect, useMemo, useState,
 } from 'react';
 import PropTypes from 'prop-types';
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem('theme');
+
+    if (!storedTheme || (storedTheme !== 'light' && storedTheme !== 'dark')) {
+      return 'light';
+    }
+
+    return storedTheme;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   const handleToggleTheme = useCallback(() => {
     setTheme((prevState) => (prevState === 'light' ? 'dark' : 'light'));
